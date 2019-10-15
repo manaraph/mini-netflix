0fb2ac628459e87b3360fb5ed110663e54aebbda
Manasseh Omachonu <manaraph225@gmail.com>
2019-10-15 23:11:51 +0100

    Generate registration component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
