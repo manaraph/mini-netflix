96ec6dfc001d158f13e3c593dce44b721db2e868
Manasseh Omachonu <manaraph225@gmail.com>
2019-10-16 00:51:35 +0100

    add register function in user service

--- frontend/src/app/_services/user.service.ts
+++ frontend/src/app/_services/user.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { IUser } from '../_models/user.model';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  register(user: IUser) {
+    const options = { headers: new HttpHeaders({'Content-Type': 'application/json'})};
+    return this.http.post<any>('/api/register', user, options);
+  }
 }
