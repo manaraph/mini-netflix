7747efcb069b559d4e4158f0b52a6fd2feccc45b
Manasseh Omachonu <manaraph225@gmail.com>
2019-10-16 01:02:45 +0100

    Some registration function

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../_services/authentication.service';
+import { UserService } from '../_services/user.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +10,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  register: FormGroup;
+  loading: false;
+  submitted: false;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private authService: AuthenticationService,
+    private userService: UserService,
+  ) {
+    // Redirect to homepage if already logged in
+    if (this.authService.isAuthenticated) {
+      this.router.navigate(['/']);
+    }
+   }
 
   ngOnInit() {
   }
 
 }
+
