6643adf579fabc482703f2eff16de6574f5ee63b
Manasseh Omachonu <manaraph225@gmail.com>
2019-10-16 02:28:55 +0100

    Fixing the navbar

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { MoviesComponent } from './movies/movies.component';
 import { MovieDetailComponent } from './movies/movie-detail/movie-detail.component';
+import { FavoritesComponent } from './movies/favorites/favorites.component';
 
 
 const routes: Routes = [
@@ -11,6 +12,7 @@ const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'movies', component: MoviesComponent },
   { path: 'movies/:id', component: MovieDetailComponent },
+  { path: 'favorites', component: FavoritesComponent },
   { path: '', redirectTo: '/movies', pathMatch: 'full' },
 ];
 
